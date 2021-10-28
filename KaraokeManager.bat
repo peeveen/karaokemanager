eee219811342fd078c92d5597d1445d06ed1d0a8
Steven Frew <steven.fullhouse@gmail.com>
2021-10-28 18:10:06 +0100

    updated batch file

--- KaraokeManager.bat
+++ KaraokeManager.bat
@@ -3,4 +3,4 @@ py -m pip install --upgrade pip
 py -m pip install colorama
 py -m pip install pyyaml
 py -m pip install textdistance
-py KaraokeManager.py "DataFilesPath=%~1" "KaraokeFilesPath=%~2" "MusicFilesPath=%~3"
+py KaraokeManager.py %~1
