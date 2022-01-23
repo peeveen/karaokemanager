56bcbae598ef9dea6e1bc5dc1c23784d1c0c4be1
Steven Frew <steven.fullhouse@gmail.com>
2022-01-23 13:39:04 +0000

    now uses raw hwnd for pacemaker window, prevents finding it twice

--- WinampDriver.py
+++ WinampDriver.py
@@ -1,6 +1,7 @@
 from os import startfile, path
 from Driver import Driver
 from win32ui import FindWindowEx
+from win32api import SendMessage
 from time import sleep
 import threading
 
@@ -30,16 +31,17 @@ class WinampDriver(Driver):
 			# Pacemaker resetting itself when a new file starts, and us sending
 			# this message to set a value, is too tight. We will set it once
 			# every second for the next five seconds.
+			# Can't pass the Pacemaker window object through, it gets sniffy
+			# about thread contexts, so just use the raw HWND.
+			hwnd=pacemakerWindow.GetSafeHwnd()
 			keyChangeThread = threading.Thread(
-				target=applyKeyChange, args=(keyChange,5,))
+				target=applyKeyChange, args=(hwnd,keyChange,5,))
 			keyChangeThread.daemon = True
 			keyChangeThread.start()
 
 # Sends the PM_SET_PITCH message to the PaceMaker window (if available) to set
 # the correct key change to whatever the user requested for a particular song.
-def applyKeyChange(keyChange,count):
-	pacemakerWindow=FindWindowEx(None, None, None, "PaceMaker Plug-in")
-	if not pacemakerWindow is None:
-		for _ in range(count):
-			pacemakerWindow.SendMessage(PM_SET_PITCH,0,keyChange*1000)
-			sleep(1)
+def applyKeyChange(pacemakerHwnd,keyChange,count):
+	for _ in range(count):
+		SendMessage(pacemakerHwnd,PM_SET_PITCH,0,keyChange*1000)
+		sleep(1)
